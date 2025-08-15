4205a52841fd7610de7d0ffd9676b271548aef36
just-a-developer-man <167205303+just-a-developer-man@users.noreply.github.com>
2025-08-15 15:14:13 +0300

    Keymaps for goplay

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_keymap("n", "<leader>gpt", ":GPToggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>gpe", ":GPExec<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>gpf", ":GPExecFile<CR>", { noremap = true, silent = true })
